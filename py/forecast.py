61d1104d566e027f4153b0c92eb1d48003cf8f56
mxblsdl <maxblasdel@gmail.com>
2021-03-14 13:58:54 -0700

    move python

--- py/forecast.py
+++ py/forecast.py
@@ -10,7 +10,7 @@ from noaa_sdk import NOAA
 
 
 # %%
-def get_observations(zip, hour):
+def get_observations(zip):
     n = NOAA()
     d = n.get_forecasts(zip, "US")
     #d = {k:o[hour][k] for k in ["isDaytime", "startTime", "temperature", "temperatureTrend", "windSpeed", "windDirection", "shortForecast"]}
